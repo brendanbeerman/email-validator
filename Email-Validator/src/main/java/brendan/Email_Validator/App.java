fd9ca77dc9888fa1e2140d165f9050ebfd0af2e8
Brendan <brendan@192.168.2.42>
2017-02-22 16:28:21 -0400

    Set up for the project

---
+++ Email-Validator/src/main/java/brendan/Email_Validator/App.java
@@ -0,0 +1,13 @@
+package brendan.Email_Validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
